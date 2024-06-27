16de9bf75ea1efa47d2923a35e92041c5c902733
Christian Haack <chr.hck@gmail.come>
2024-06-27 17:55:46 +0200

    initial commit

--- src/AbstractMediumProperties.jl
+++ src/AbstractMediumProperties.jl
@@ -1,5 +1,106 @@
 module AbstractMediumProperties
 
-# Write your package code here.
+export MediumProperties
+export pressure, temperature, material_density, mean_scattering_angle
+export scattering_length, absorption_length, group_refractive_index, phase_refractive_index
+export dispersion, cherenkov_angle, group_velocity
 
+const c_vac_m_ns = 0.299792458
+
+abstract type MediumProperties end
+
+_not_implemented(medium) = error("Not implemented for type $(typeof(medium))")
+
+"""
+    pressure(::MediumProperties) 
+
+This function returns the pressure for a given medium.
+"""
+pressure(medium::MediumProperties) = _not_implemented(medium)
+temperature(medium::MediumProperties) = _not_implemented(medium) 
+material_density(medium::MediumProperties) = _not_implemented(medium) 
+mean_scattering_angle(medium::MediumProperties) = _not_implemented(medium)
+
+
+"""
+    scattering_function(medium::MediumProperties)
+
+Return a scattering angle sampled from the scattering function of the medium.
+
+"""
+scattering_function(medium::MediumProperties) = _not_implemented(medium)
+
+"""
+    scattering_length(wavelength, medium::MediumProperties) 
+Return scattering length at `wavelength` in units m.
+
+`wavelength` is expected to be in units nm. Returned length is in units m.
+"""
+scattering_length(wavelength, medium::MediumProperties) = _not_implemented(medium)
+
+
+"""
+    absorption_length(wavelength, medium::MediumProperties)
+Return absorption length at `wavelength` in units m.
+
+`wavelength` is expected to be in units nm.
+"""
+absorption_length(wavelength, medium::MediumProperties) = _not_implemented(medium)
+
+
+
+"""
+    group_refractive_index(wavelength, medium)
+Return the group refractive index at `wavelength`.
+
+`wavelength` is expected to be in units nm.
+"""
+group_refractive_index(wavelength, medium::MediumProperties) = _not_implemented(medium)
+
+"""
+    phase_refractive_index(wavelength, medium)
+Return the phase refractive index at `wavelength`.
+
+`wavelength` is expected to be in units nm.
+"""
+phase_refractive_index(wavelength, medium::MediumProperties) = _not_implemented(medium)
+
+
+"""
+    dispersion(wavelength, medium)
+Return the dispersion dn/dλ at `wavelength` in units 1/nm.
+
+`wavelength` is expected to be in units nm.
+"""
+dispersion(wavelength, medium::MediumProperties) = _not_implemented(medium)
+
+
+"""
+    cherenkov_angle(wavelength, medium::MediumProperties)
+Calculate the cherenkov angle (in rad) for `wavelength`.
+
+`wavelength` is expected to be in units nm.
+"""
+function cherenkov_angle(wavelength, medium::MediumProperties)
+    return acos(one(typeof(wavelength)) / phase_refractive_index(wavelength, medium))
 end
+
+"""
+    group_velocity(wavelength, medium)
+Return the group_velocity in m/ns at `wavelength`.
+
+`wavelength` is expected to be in units nm.
+"""
+function group_velocity(wavelength, medium::MediumProperties)
+    global c_vac_m_ns
+    T = typeof(wavelength)
+
+    # Explicitely convert everything to the type of wavelength
+    # This ise useful to avoud double precision if this function is called in a CUDA kernel
+
+    ref_ix::T = phase_refractive_index(wavelength, medium)
+    λ_0::T = ref_ix * wavelength
+    T(c_vac_m_ns) / (ref_ix - λ_0 * dispersion(wavelength, medium))
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,44 @@
 using AbstractMediumProperties
 using Test
 
+
+
+function test_interface(medium::MediumProperties)
+    wavelength = 400.
+
+    @test pressure(medium) isa Number
+    @test temperature(medium) isa Number
+    @test material_density(medium) isa Number
+    @test mean_scattering_angle(medium) isa Number
+    @test scattering_length(wavelength, medium) isa Number
+    @test absorption_length(wavelength, medium) isa Number
+    @test group_refractive_index(wavelength, medium) isa Number
+    @test phase_refractive_index(wavelength, medium) isa Number
+    @test dispersion(wavelength, medium) isa Number
+    @test cherenkov_angle(wavelength, medium) isa Number
+    @test group_velocity(wavelength, medium) isa Number
+    @test scattering_function(wavelength, medium) isa Number
+end
+
+struct MockMediumProperties <: MediumProperties end
+
+AbstractMediumProperties.pressure(medium::MockMediumProperties) = 1.
+AbstractMediumProperties.temperature(medium::MockMediumProperties) = 2.
+AbstractMediumProperties.material_density(medium::MockMediumProperties) = 3.
+AbstractMediumProperties.mean_scattering_angle(medium::MockMediumProperties) = 4.
+AbstractMediumProperties.scattering_function(medium::MockMediumProperties) = π
+AbstractMediumProperties.scattering_length(wavelength, medium::MockMediumProperties) = 5.
+AbstractMediumProperties.absorption_length(wavelength, medium::MockMediumProperties) = 6.
+AbstractMediumProperties.group_refractive_index(wavelength, medium::MockMediumProperties) = 1.2
+AbstractMediumProperties.phase_refractive_index(wavelength, medium::MockMediumProperties) = 1.1
+AbstractMediumProperties.dispersion(wavelength, medium::MockMediumProperties) = 0.001
+
+
 @testset "AbstractMediumProperties.jl" begin
-    # Write your tests here.
+    medium = MockMediumProperties()
+    test_interface(medium)
+
+    wavelength = 400.
+    @test group_velocity(400., medium) ≈ 0.45423099697
+    @test cherenkov_angle(400., medium) ≈ 0.429699666
 end
